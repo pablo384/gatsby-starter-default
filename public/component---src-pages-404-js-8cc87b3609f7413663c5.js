29bb2f009449b5e651773cb76d1e8814e6ed50d5
Pablo Reinoso <pabloreinoso0@gmail.com>
2024-06-14 16:15:40 -0400

    gitlab Runner added

--- public/component---src-pages-404-js-8cc87b3609f7413663c5.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[125],{3331:function(t,e,n){n.r(e),n.d(e,{Head:function(){return s}});var a=n(6540),u=n(810),l=n(7528);const s=()=>a.createElement(l.A,{title:"404: Not Found"});e.default=()=>a.createElement(u.A,null,a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
-//# sourceMappingURL=component---src-pages-404-js-8cc87b3609f7413663c5.js.map
